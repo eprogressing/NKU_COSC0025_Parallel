e90117c8b28568d391b52d7848d5b2923cd6e65e
eprogressing <167550629+eprogressing@users.noreply.github.com>
2025-04-27 07:32:34 +0800

    Create ordinary.cc

---
+++ lab2/ordinary.cc
@@ -0,0 +1,74 @@
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <iomanip>
+#include <sys/time.h>
+// #include <omp.h>
+#include <arm_neon.h>
+// 可以自行添加需要的头文件
+
+void fRead(int *a, int *b, int *n, int *p, int input_id){
+    // 数据输入函数
+    std::string str1 = "/nttdata/";
+    std::string str2 = std::to_string(input_id);
+    std::string strin = str1 + str2 + ".in";
+    char data_path[strin.size() + 1];
+    std::copy(strin.begin(), strin.end(), data_path);
+    data_path[strin.size()] = '\0';
+    std::ifstream fin;
+    fin.open(data_path, std::ios::in);
+    fin>>*n>>*p;
+    for (int i = 0; i < *n; i++){
+        fin>>a[i];
+    }
+    for (int i = 0; i < *n; i++){   
+        fin>>b[i];
+    }
+}
+
+void fCheck(int *ab, int n, int input_id){
+    // 判断多项式乘法结果是否正确
+    std::string str1 = "/nttdata/";
+    std::string str2 = std::to_string(input_id);
+    std::string strout = str1 + str2 + ".out";
+    char data_path[strout.size() + 1];
+    std::copy(strout.begin(), strout.end(), data_path);
+    data_path[strout.size()] = '\0';
+    std::ifstream fin;
+    fin.open(data_path, std::ios::in);
+    for (int i = 0; i < n * 2 - 1; i++){
+        int x;
+        fin>>x;
+        if(x != ab[i]){
+            std::cout<<"多项式乘法结果错误"<<std::endl;
+            return;
+        }
+    }
+    std::cout<<"多项式乘法结果正确"<<std::endl;
+    return;
+}
+
+void fWrite(int *ab, int n, int input_id){
+    // 数据输出函数, 可以用来输出最终结果, 也可用于调试时输出中间数组
+    std::string str1 = "files/";
+    std::string str2 = std::to_string(input_id);
+    std::string strout = str1 + str2 + ".out";
+    char output_path[strout.size() + 1];
+    std::copy(strout.begin(), strout.end(), output_path);
+    output_path[strout.size()] = '\0';
+    std::ofstream fout;
+    fout.open(output_path, std::ios::out);
+    for (int i = 0; i < n * 2 - 1; i++){
+        fout<<ab[i]<<'\n';
+    }
+}
+
+void poly_multiply(int *a, int *b, int *ab, int n, int p){
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j){
+            ab[i+j]=(1LL * a[i] * b[j] % p + ab[i+j]) % p;
+        }
+    }
+}
